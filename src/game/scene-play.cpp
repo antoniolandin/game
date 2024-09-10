52f9367afb9f4c44c000349ee0b5a242bcc633e2
Pastafarista <antoniolandin@proton.me>
2024-09-10 23:44:05 +0200

    test: RenderAABB system

--- src/game/scene-play.cpp
+++ src/game/scene-play.cpp
@@ -1,15 +1,68 @@
 #include "scene-play.h"
+#include "components/rect-shape.h"
 #include "game/game-engine.h"
 #include <SFML/Window/Keyboard.hpp>
+#include "components/transform.h"
+#include "components/aabb.h"
+#include "components/renderable.h"
+#include "systems/render-aabb.h"
 
 ScenePlay::ScenePlay(GameEngine* game_engine)
     : Scene(game_engine)
 {
+    // initialize coordinator
+    m_coordinator.init();
+    
+    // register actions, components and systems
+    registration(); 
+
+    // spawn player
+    spawnPlayer();
+}
+
+void ScenePlay::registration()
+{
+    // register actions
     registerAction(sf::Keyboard::Escape, "QUIT");
+
+    // register components
+    m_coordinator.registerComponent<Transform>();
+    m_coordinator.registerComponent<AABB>();
+    m_coordinator.registerComponent<RectShape>();
+    m_coordinator.registerComponent<Renderable>();
+
+    // register systems
+    m_render_aabb_system = m_coordinator.registerSystem<RenderAABB>();
+    {
+        Signature signature;
+        signature.set(m_coordinator.getComponentType<RectShape>());
+        signature.set(m_coordinator.getComponentType<AABB>());
+        signature.set(m_coordinator.getComponentType<Transform>());
+        signature.set(m_coordinator.getComponentType<Renderable>());
+        m_coordinator.setSystemSignature<RenderAABB>(signature);
+    }
+
+    m_render_aabb_system->init();
+}
+
+void ScenePlay::spawnPlayer()
+{
+    Entity player = m_coordinator.createEntity();
+
+    Vec2 player_size = Vec2(64, 64);
+    Vec2 window_center = Vec2(width()/2.f, height()/2.f);
+
+    m_coordinator.addComponent(player, AABB{player_size, player_size/2});
+    m_coordinator.addComponent(player, Transform{window_center, Vec2(0,0)});
+    m_coordinator.addComponent(player, RectShape{});
+    m_coordinator.addComponent(player, Renderable{&m_game_engine->window()});
+    
+    m_render_aabb_system->initEntity(player);
 }
 
 void ScenePlay::update()
 {
+    m_render_aabb_system->update();
 }
 
 void ScenePlay::doAction(const Action& action)
@@ -25,6 +78,7 @@ void ScenePlay::doAction(const Action& action)
     {
     }
 }
+
 
 void ScenePlay::render()
 {
--- src/game/scene-play.h
+++ src/game/scene-play.h
@@ -1,9 +1,18 @@
+#pragma once
+
 #include "scene.h"
+#include "systems/render-aabb.h"
 
 class ScenePlay : public Scene {
 private:
-    void registerActions();
     Coordinator m_coordinator;
+    std::vector<Entity> m_entities;
+
+    // systems
+    std::shared_ptr<RenderAABB> m_render_aabb_system;
+
+    void registration();
+    void spawnPlayer();
 
 public:
     ScenePlay(GameEngine* game_engine);
