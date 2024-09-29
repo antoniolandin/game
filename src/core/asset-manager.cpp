277f96a3488f66103679152a220a1f6411b7b3b8
antoniolandin <antoniolandin@proton.me>
2024-09-29 23:13:48 +0200

    feat: started animation loading system from json files

--- src/core/asset-manager.cpp
+++ src/core/asset-manager.cpp
@@ -6,6 +6,30 @@ void AssetManager::addTexture(const std::string& name, const std::string& path) 
     m_textures[name] = texture;
 }
 
+// get the sprite from the zombie tileset (it has weird spacing)
+sf::IntRect getSpriteRectFromZombieTileset(const int x, const int y)
+{
+    // define the tileset properties
+    const int tile_width = 16;
+    const int tile_height = 16;
+    const int tile_spacing_x = 1;
+    const float tile_spacing_y = 3;
+    const float scale = 4;
+
+    // calculate the position of the sprite in the tileset
+    const int posX = (x * tile_width) + (x * tile_spacing_x);
+    const int posY = (y * tile_height) + (y * tile_spacing_y);
+
+    // generate the sprite from the tileset using the position and size
+    return sf::IntRect(posX, posY, tile_width, tile_height);
+}
+
+void AssetManager::loadAnimation(const Entity& entity, const std::string& name, const std::string& path) {
+    sf::Texture texture;
+    texture.loadFromFile(path);
+    m_textures[name] = texture;
+}
+
 sf::Texture& AssetManager::getTexture(const std::string& name) {
     return m_textures.at(name);
 }
--- src/core/asset-manager.h
+++ src/core/asset-manager.h
@@ -1,12 +1,16 @@
 #pragma once
+
+#include "core/types.h"
+#include <SFML/Graphics.hpp>
 #include <map>
 #include <string>
-#include <SFML/Graphics.hpp>
 
 class AssetManager {
 private:
     std::map<std::string, sf::Texture> m_textures;
+
 public:
     void addTexture(const std::string& name, const std::string& path);
+    void loadAnimation(const Entity& entity, const std::string& name, const std::string& path);
     sf::Texture& getTexture(const std::string& name);
 };
